f6e707667c4cd64a1951a6535db8a05205357bd8
Tom Scanlan <tompscanlan@gmail.com>
2024-08-31 09:08:50 -0400

    search and delete by creator id

--- src-tauri/src/event.rs
+++ src-tauri/src/event.rs
@@ -204,6 +204,57 @@ pub async fn list_events() -> Result<Vec<Event>> {
     Ok(events)
 }
 
+#[tauri::command]
+pub async fn list_events_by_creator_id(creator_id: Uuid) -> Result<Vec<Event>> {
+    let mut cluster = init_cluster().await?;
+    let session = cluster.connect().await.map_err(|e| e.to_string())?;
+    let db_name = "openmeet.events";
+
+    let query = format!("SELECT * FROM {} WHERE creator_id = ?;", db_name);
+    let mut statement = session.statement(query.clone());
+    statement.bind_by_name("creator_id", creator_id)?;
+    let result = statement.execute().await.map_err(|e| e.to_string())?;
+
+    let mut events = Vec::new();
+
+    let mut iterator = result.iter();
+
+    while let Some(row) = iterator.next() {
+        let event = Event {
+            event_id: row.get_column_by_name("event_id").and_then(|v| v.get_uuid()).map_err(|e| e.to_string())?,
+            creator_id: row.get_column_by_name("creator_id").and_then(|v| v.get_uuid()).map_err(|e| e.to_string())?,
+            title: row.get_column_by_name("title").and_then(|v| v.get_string()).map_err(|e| e.to_string())?,
+            description: row.get_column_by_name("description").and_then(|v| v.get_string()).map_err(|e| e.to_string())?,
+            start_time: row.get_column_by_name("start_time").and_then(|v| v.get_i64()).map_err(|e| e.to_string())?,
+            end_time: row.get_column_by_name("end_time").and_then(|v| v.get_i64()).map_err(|e| e.to_string())?,
+            lat: row.get_column_by_name("lat").and_then(|v| v.get_f64()).map_err(|e| e.to_string())?,
+            lon: row.get_column_by_name("lon").and_then(|v| v.get_f64()).map_err(|e| e.to_string())?,
+            address: row.get_column_by_name("address").and_then(|v| v.get_string()).map_err(|e| e.to_string())?,
+            created_at: row.get_column_by_name("created_at").and_then(|v| v.get_i64()).map_err(|e| e.to_string())?,
+            updated_at: row.get_column_by_name("updated_at").and_then(|v| v.get_i64()).map_err(|e| e.to_string())?,
+        };
+        events.push(event);
+    }
+
+    Ok(events)
+}
+
+
+#[tauri::command]
+pub async fn delete_events_by_creator_id(creator_id: Uuid) -> Result<()> {
+    let mut cluster = init_cluster().await?;
+    let session = cluster.connect().await.map_err(|e| e.to_string())?;
+    let db_name = "openmeet.events";
+
+    let query = format!("DELETE FROM {} WHERE creator_id = ?;", db_name);
+    let mut statement = session.statement(query.clone());
+    statement.bind_by_name("creator_id", creator_id)?;
+
+    statement.execute().await.map_err(|e| e.to_string())?;
+
+    Ok(())
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -267,6 +318,29 @@ mod tests {
         let event = get_event(creator_id, event.start_time, event.event_id).await.unwrap();
 
         assert_eq!(event.unwrap().title, "title is good");
+    }
+
+    #[tokio::test]
+    async fn test_delete_events_by_creator_id() {
+        // select a random creator_id
+        let events = list_events().await.unwrap();
+        if events.is_empty() {
+            return;
+        }
+        let creator_id = events[0].creator_id;
         
+        // check inital record count
+        let events = list_events_by_creator_id(creator_id).await.unwrap();
+        let events_count = events.len();
+        assert!(events_count > 0);
+
+        // delete all events with that creator_id
+        delete_events_by_creator_id(creator_id).await.unwrap();
+
+        // assert that the count of events is 0 for that creator_id
+        let events_for_creator = list_events_by_creator_id(creator_id).await.unwrap();
+        assert_eq!(events_for_creator.len(), 0);
+
+
     }
 }
